f8ef079a463f49e325cc77da83efb8e18026680e
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-07-08 23:46:54 +0300

    the function of saving and receiving information from localStorage

--- src/js/total-js/shop-list-di.js
+++ src/js/total-js/shop-list-di.js
@@ -1,12 +1,65 @@
 
+const shopDiv = document.querySelector('.shop-background');
 
-function safeLokalStor() {
-    const info = {
-        autor: 'dima',
-        janr: 'fantazi'
-    };
+
+const infoBooks = [];
+localStorage.setItem('infoBooks', JSON.stringify(infoBooks));
+
+
+
+
+const test = {
+  author:'dima',
+  book_image:'bla-bla-bla.ua',
+  title:'dream team',
+  list_name:'fantastika',
+  description:'there will be a lot of text for the book description field test…',
+};
+
+const куіе = {
+  author:'dima',
+  book_image:'bla-bla-bla.ua',
+  title:'dream team',
+  list_name:'fantastika',
+  description:'there will be a lot of text for the book description field test…',
+};
+
+
+
+
+
+
+function saveLokalStor(info) {
+
+   const infoBooksString = localStorage.getItem('infoBooks');
+   const infoBooks = JSON.parse(infoBooksString) || [];
+
+   infoBooks.push(info);
+
+   localStorage.setItem('infoBooks', JSON.stringify(infoBooks));
     
-    localStorage.setItem('book', JSON.stringify(info));
 }
 
-safeLokalStor();
+saveLokalStor(test);
+saveLokalStor(test);
+
+
+// ховае блок через додавання класу
+//  shopDiv.classList.add('displNan');
+
+
+
+
+function drawingMarkings() {
+  const infoBooksString = localStorage.getItem('infoBooks');
+  const infoBooks = JSON.parse(infoBooksString);
+
+  if (!infoBooks || infoBooks.length === 0) {
+    console.log('Пусто');
+  } else {
+    console.log(infoBooks);
+  }
+}
+
+
+drawingMarkings();
