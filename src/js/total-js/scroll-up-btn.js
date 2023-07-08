a93bbb4369c4c471b374a7e82220c09a19d2bea3
Dmytro Savitskyi <zefir0211@gmail.com>
2023-07-09 00:48:12 +0200

    fix

--- src/js/total-js/scroll-up-btn.js
+++ src/js/total-js/scroll-up-btn.js
@@ -1,24 +1,25 @@
-const scrollTop = document.querySelector('#scroll-top');
-
-window.addEventListener('scroll', onScrollToTop);
-window.addEventListener('load', () => {
-  window.scrollTo(0, 0);
-});
-scrollTop.addEventListener('click', onScrollToTopClick);
-
-function onScrollToTop() {
-  window.onscroll = () => {
-    if (window.scrollY > 700) {
-      scrollTop.classList.remove('js-scroll-up_hidden');
-    } else if (window.scrollY < 700) {
-      scrollTop.classList.add('js-scroll-up_hidden');
+function initScrollToTop() {
+    const scrollTop = document.querySelector('#scroll-top');
+    scrollTop.addEventListener('click', onScrollToTopClick);
+  
+    window.addEventListener('scroll', onScroll);
+  
+    function onScroll() {
+      const scrollPosition = window.scrollY;
+      const threshold = 700;
+      scrollTop.classList.toggle('js-scroll-up_hidden', scrollPosition < threshold);
     }
-  };
-}
-
-function onScrollToTopClick() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
+  
+    function onScrollToTopClick() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  
+    window.addEventListener('load', () => {
+      window.scrollTo(0, 0);
+    });
+  }
+  
+  initScrollToTop();
