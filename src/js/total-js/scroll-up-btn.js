b4211ca88e4d9381cb07c670f57a8c8e77a5e385
Dmytro Savitskyi <zefir0211@gmail.com>
2023-07-09 01:05:13 +0200

    scroll-btn+fix
    Зробив кнопку вгору + пофіксив секцію support, неправильні силки на сайт були починаючи з 4-ї

--- src/js/total-js/scroll-up-btn.js
+++ src/js/total-js/scroll-up-btn.js
@@ -1,25 +1,25 @@
 function initScrollToTop() {
     const scrollTop = document.querySelector('#scroll-top');
     scrollTop.addEventListener('click', onScrollToTopClick);
-  
+
     window.addEventListener('scroll', onScroll);
-  
-    function onScroll() {
-      const scrollPosition = window.scrollY;
-      const threshold = 700;
-      scrollTop.classList.toggle('js-scroll-up_hidden', scrollPosition < threshold);
+
+function onScroll() {
+    const scrollPosition = window.scrollY;
+    const threshold = 700;
+    scrollTop.classList.toggle('js-scroll-up_hidden', scrollPosition < threshold);
     }
-  
+
     function onScrollToTopClick() {
-      window.scrollTo({
+        window.scrollTo({
         top: 0,
         behavior: 'smooth',
-      });
-    }
-  
-    window.addEventListener('load', () => {
-      window.scrollTo(0, 0);
     });
-  }
-  
-  initScrollToTop();
+    }
+
+window.addEventListener('load', () => {
+    window.scrollTo(0, 0);
+    });
+}
+
+initScrollToTop();
