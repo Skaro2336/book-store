7d9537679bf455563b934a4f4ce8d0c0c582737c
Dmytro Savitskyi <zefir0211@gmail.com>
2023-07-09 16:59:03 +0200

    fix url suport-ukrain.js,suport-ukrain.html, inst.libr

--- src/js/total-js/support-ukraine.js
+++ src/js/total-js/support-ukraine.js
@@ -1,5 +1,4 @@
 import supportUkraineList from "./support-ukraine-list";
-import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider"
 
 const supportUkraineListEl = document.querySelector('.support-list');
 const downBtn = document.querySelector('.support-down-btn');
