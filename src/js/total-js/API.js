78c8cd9b8ab688ed652ec5b46814dacd96a0bad7
Liu3oIT <liutyivova995@gmail.com>
2023-07-10 18:18:19 +0300

    a

--- src/js/total-js/API.js
+++ src/js/total-js/API.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-
 export async function fetchCategoryBooks() {
   const BASE_URL = 'https://books-backend.p.goit.global/books/category-list';
-
   try {
     const response = await axios.get(BASE_URL);
-
     if (response.status !== 200) {
       throw new Error(`Request failed with status ${response.status}`);
     }
-
-    return response.data;
+    return response.data.map(item => ({
+      list_name: item.list_name,
+    }));
   } catch (error) {
     Notiflix.Notify.warning(
       `Oops! Something went wrong. You caught the following error: ${error.message}.`
@@ -22,7 +20,6 @@ export async function fetchTopFiveBooks() {
   const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
   try {
     const response = await axios.get(BASE_URL);
-    console.log();
     if (response.status !== 200) {
       throw new Error(`Request failed with status ${response.status}`);
     }
