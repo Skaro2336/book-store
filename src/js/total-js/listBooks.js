1edc2b2a85b6ccaa3713c26f4cef56cb2146d29d
Liu3oIT <liutyivova995@gmail.com>
2023-07-10 16:39:39 +0300

    fixAPI

--- src/js/total-js/listBooks.js
+++ src/js/total-js/listBooks.js
@@ -1,40 +1,17 @@
 import { fetchCategoryBooks, fetchTopFiveBooks } from './API';
-
-listForCategory();
-topBooks();
-async function listForCategory() {
-  try {
-    const response = await fetchCategoryBooks();
-
-    const listName = response.map(info => ({
-      titleName: info.list_name,
-    }));
-
-    markapCategoryList(listName);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 function markapCategoryList(listName) {
   const listCategoryBooks = document.querySelector('.list-category-books');
   listName.map(e => {
     const listCategory = `
-  <li>${e.titleName}</li>
-`;
+      <li>${e.titleName}</li>
+    `;
     listCategoryBooks.insertAdjacentHTML('beforeend', listCategory);
   });
 }
-async function topBooks() {
-  try {
-    const response = await fetchTopFiveBooks();
-    console.log(response);
-    markupCategoryList(response);
-    function markupCategoryList(books) {
-      const list = document.querySelector('.books-container');
-
-      books.forEach(category => {
-        const titleCategory = `
+function markupCategoryList(books) {
+  const list = document.querySelector('.books-container');
+  books.forEach(category => {
+    const titleCategory = `
       <div class= "section-category-for-books ">
         <h2 class="title-category-name">${category.list_name}</h2>
         <div class= "section-books">
@@ -42,29 +19,45 @@ async function topBooks() {
             .map(book => {
               return `
                 <li>
-                <a href="#"  data-id="${book._id}">
-                  <div class = "card-book">
-                    <img class = "books-card-img" src="${book.book_image}" alt="${book.title}"    width = "180";
-                    height = "256" loading="lazy">
-                  </div>
-                  <div class="info-books">
-                        <h3 class="title-books">${book.title}</h3>
-                        <p class="name-author">${book.author}</p>
-                      </div>
-                    </a>
+                  <a href="#" data-id="${book._id}">
+                    <div class="card-book">
+                      <img class="books-card-img" src="${book.book_image}" alt="${book.title}" width="180" height="256" loading="lazy">
+                    </div>
+                    <div class="info-books">
+                      <h3 class="title-books">${book.title}</h3>
+                      <p class="name-author">${book.author}</p>
+                    </div>
+                  </a>
                 </li>
               `;
             })
             .join('')}
-       </div>
-       <button class= "books-btn" type ="button">see more</button>
+        </div>
+        <button class="books-btn" type="button">see more</button>
       </div>
     `;
-
-        list.insertAdjacentHTML('beforeend', titleCategory);
-      });
-    }
+    list.insertAdjacentHTML('beforeend', titleCategory);
+  });
+}
+async function listForCategory() {
+  try {
+    const response = await fetchCategoryBooks();
+    const listName = response.map(info => ({
+      titleName: info.list_name,
+    }));
+    markapCategoryList(listName);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+async function topBooks() {
+  try {
+    const response = await fetchTopFiveBooks();
+    console.log(response);
+    markupCategoryList(response);
   } catch (error) {
     console.log(error);
   }
 }
+listForCategory();
+topBooks();
